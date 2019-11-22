fc4d99dd45f2f89b1acaee2d82ec059a5981b986
JQuiles-MBGE <jquiles@mbge.com.mx>
2019-11-22 15:07:05 -0600

    Test 3

--- src/App.js
+++ src/App.js
@@ -8,7 +8,7 @@ function App() {
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
-          Hola Migue, como estas?
+          Hola Javi, como estas?
         </p>
         <a
           className="App-link"
